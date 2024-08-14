6e714821bd21300361df8c6b5a72ad606b64eadd
Martim Esteves <fimaales@gmail.com>
2024-08-14 17:17:23 +0100

    module iob_diff generated with py2hwsw

--- lib/hardware/modules/iob_diff/iob_diff.py
+++ lib/hardware/modules/iob_diff/iob_diff.py
@@ -3,11 +3,97 @@ def setup(py_params_dict):
         "original_name": "iob_diff",
         "name": "iob_diff",
         "version": "0.1",
-        "generate_hw": False,
+        "confs": [
+            {
+                "name": "DATA_W",
+                "type": "P",
+                "val": "32",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Data bus width",
+            },
+            {
+                "name": "RST_VAL",
+                "type": "P",
+                "val": 0,
+                "min": "NA",
+                "max": "NA",
+                "descr": "Reset value.",
+            },
+        ],
+        "ports": [
+            {
+                "name": "clk_en_rst",
+                "interface": {
+                    "type": "clk_en_rst",
+                    "subtype": "slave",
+                },
+                "descr": "Clock, clock enable and reset",
+            },
+            {
+                "name": "rst_i",
+                "descr": "Input port",
+                "signals": [
+                    {
+                        "name": "rst",
+                        "width": 1,
+                        "direction": "input",
+                    },
+                ],
+            },
+            {
+                "name": "data_i",
+                "descr": "Input port",
+                "signals": [
+                    {
+                        "name": "data",
+                        "width": "DATA_W",
+                        "direction": "input",
+                    },
+                ],
+            },
+            {
+                "name": "data_o",
+                "descr": "Output port",
+                "signals": [
+                    {
+                        "name": "data",
+                        "width": "DATA_W",
+                        "direction": "output",
+                    },
+                ],
+            },
+        ],
+        "wires": [
+            {
+                "name": "data_int_reg",
+                "descr": "data_int_reg wire",
+                "signals": [
+                    {"name": "data_int_reg", "width": "DATA_W"},
+                ],
+            },
+        ],
         "blocks": [
             {
                 "core_name": "iob_reg_r",
-                "instance_name": "iob_reg_r_inst",
+                "instance_name": "reg0",
+                "parameters": {
+                    "DATA_W": "DATA_W",
+                    "RST_VAL": "RST_VAL",
+                },
+                "connect": {
+                    "clk_en_rst": "clk_en_rst",
+                    "rst": "rst_i",
+                    "data_i": "data_i",
+                    "data_o": "data_int_reg",
+                },
+            },
+        ],
+        "snippets": [
+            {
+                "verilog_code": """
+        assign data_o = data_i - data_int_reg;
+            """,
             },
         ],
     }
